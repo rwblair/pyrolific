03491e4a9450c0295d2ddb4d9c7cb2acaa431d7e
Ross Blair <rosswilsonblair@gmail.com>
2024-11-18 16:19:17 -0600

    add publishing to short status model.

--- pyrolific/models/study_short_status.py
+++ pyrolific/models/study_short_status.py
@@ -8,6 +8,7 @@ class StudyShortStatus(str, Enum):
     PAUSED = "PAUSED"
     SCHEDULED = "SCHEDULED"
     UNPUBLISHED = "UNPUBLISHED"
+    PUBLISHING = "PUBLISHING"
 
     def __str__(self) -> str:
         return str(self.value)
